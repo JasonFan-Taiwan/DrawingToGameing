301ab21d4ba3463aa42d236244e9773ec87b9e1b
JasonFan-Taiwan <uochau@gmail.com>
2026-04-21 22:11:50 -0700

    Create config.py with application configuration and object categories

---
+++ config.py
@@ -0,0 +1,15 @@
+# Application Configuration
+
+# General settings
+APPLICATION_NAME = "DrawingToGaming"
+APPLICATION_VERSION = "1.0"
+DEBUG = True
+
+# Object Categories
+OBJECT_CATEGORIES = [
+    "Characters",
+    "Environments",
+    "Items",
+    "Animations",
+    "Sounds"
+]
